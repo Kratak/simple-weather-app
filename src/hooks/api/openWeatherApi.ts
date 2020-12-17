f193fdd30e05641b6b1eae765f349aae63414149
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2020-12-17 10:09:18 +0100

    + Add #1 browser geolocation

--- src/hooks/api/openWeatherApi.ts
+++ src/hooks/api/openWeatherApi.ts
@@ -9,6 +9,6 @@ const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
 const baseUrl = process.env.REACT_APP_OPENWEATHER_API_URL;
 
 export const openWeatherApiInstance = axios.create({
-  baseURL: `${baseUrl}${openWeatherApiCallTypes.onecall}?appid=${apiKey}&exclude=hourly,minutely`,
+  baseURL: `${baseUrl}${openWeatherApiCallTypes.onecall}?appid=${apiKey}&exclude=hourly,minutely,`,
   responseType: "json",
 });
--- src/hooks/use-api.ts
+++ src/hooks/use-api.ts
@@ -9,34 +9,57 @@ interface IGeoCoordinates {
   lat: number;
 }
 
-const mockData2 = {
-  city: "Warszawa",
-  lang: "pl",
-  units: "metric",
-};
+enum UnitsTypes {
+  metric = "metric",
+  imperial = "imperial",
+}
 
 export const useApi = (props?: IUseOpenweathermapProps) => {
   const [apiData, setApiData] = useState<{}>();
   const [geoCoordinates, setGeoCoordinates] = useState<IGeoCoordinates>();
+  const [city, setCity] = useState<string>();
+  const [unit, setUnit] = useState<UnitsTypes>(UnitsTypes.metric);
+  const [language, setLanguage] = useState<string>("pl");
+
+  const handleCityChange = (value: string) => setCity(value);
+  const handleUnitChange = (value: UnitsTypes) => setUnit(value);
+  const handleLanguageChange = (value: string) => setLanguage(value);
 
   useEffect(() => {
-    if (geoCoordinates) {
+    if (!geoCoordinates) {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords: { latitude, longitude } }) =>
+          setGeoCoordinates({ lat: latitude, lng: longitude })
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (geoCoordinates && language && unit) {
       openWeatherApiInstance
         .get(
-          `&lat=${geoCoordinates.lat}&lon=${geoCoordinates.lng}&lang=${mockData2.lang}&units=${mockData2.units}`
+          `&lat=${geoCoordinates.lat}&lon=${geoCoordinates.lng}&lang=${language}&units=${unit}`
         )
         .then((response) => setApiData(response.data))
         .catch((e) => console.log(e));
     }
-  }, [geoCoordinates]);
+  }, [geoCoordinates, language, unit]);
 
   useEffect(() => {
-    openCageApiInstance
-      .get(`&q=${mockData2.city}&language=${mockData2.lang}`)
-      .then((response) => setGeoCoordinates(response.data.results[0].geometry))
-      .catch((e) => console.log(e));
-  }, []);
+    if (city && language) {
+      openCageApiInstance
+        .get(`&q=${city}&language=${language}`)
+        .then((response) =>
+          setGeoCoordinates(response.data.results[0].geometry)
+        )
+        .catch((e) => console.log(e));
+    }
+  }, [city, language]);
+
   return {
     apiData,
+    handleCityChange,
+    handleUnitChange,
+    handleLanguageChange,
   };
 };
